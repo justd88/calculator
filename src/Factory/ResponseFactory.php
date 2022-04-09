0f47ba24288ed44cfb7cfb291e90789335ab3918
Dominik Nsosso <dominik.nsosso@immediate.co.uk>
2022-04-09 12:38:15 +0200

    add responseFactory

---
+++ src/Factory/ResponseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Factory;
+
+use App\Utils\CalculatorInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ResponseFactory
+{
+    public function __construct(
+        private CalculatorInterface  $calculator,
+    )
+    {
+    }
+
+    public function create(float $firstNumber, float $secondNumber, string $operand): JsonResponse
+    {
+        try {
+            $result = $this->calculator->calculate($firstNumber, $secondNumber, $operand);
+            return new JsonResponse(['result' => $result]);
+        } catch (\Throwable $e) {
+            return new JsonResponse(['error' => $e->getMessage()]);
+        }
+    }
+}
